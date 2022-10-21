074c956380dea04b5b49ffd9ffae2f298a3ce783
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-10-20 21:20:52 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console/chessboard/ChessPiece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xadrez_console.tabuleiro.Enums;
+
+namespace xadrez_console.tabuleiro
+{
+    public class ChessPiece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int Movimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+
+    }
+}
---
+++ xadrez-console/chessboard/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line +
+                   ", " +
+                   Column;
+        }
+    }
+}
